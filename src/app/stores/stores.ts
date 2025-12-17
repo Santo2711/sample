3c6b7bdf2f5d6e06fa4e1c22790cdbba9e125c91
Santo2711 <santodevil2711@gmail.com>
2025-12-17 19:38:41 +0530

    Store and about update

--- src/app/stores/stores.ts
+++ src/app/stores/stores.ts
@@ -1,11 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-stores',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './stores.html',
   styleUrl: './stores.css',
 })
 export class Stores {
+  
+  searchText = '';
+
+  stores = [
+    {
+      name: 'Store 1',
+      city: 'Chennai',
+      state: 'Tamil Nadu',
+      manager: 'xxxxx xxxx',
+      phone: '+91 xxxxx xxxxx',
+      status: 'Active'
+    },
+    {
+      name: 'Store 2',
+      city: 'Hyderabad',
+      state: 'Andhra Pradesh',
+      manager: 'xxxxx xxxx',
+      phone: '+91 xxxxx xxxxx',
+      status: 'Active'
+    },
+    {
+      name: 'Store 3',
+      city: 'Bengaluru',
+      state: 'Karnataka',
+      manager: 'xxxxx xxxx',
+      phone: '+91 xxxxx xxxxx',
+      status: 'Active'
+    }
+  ];
+
+  filteredStores() {
+    return this.stores.filter(store =>
+      store.name.toLowerCase().includes(this.searchText.toLowerCase()) ||
+      store.city.toLowerCase().includes(this.searchText.toLowerCase()) ||
+      store.manager.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
 
 }
+
+
